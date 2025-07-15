48b4b93ef7a8d96da1eaf8d73ff6fb37783db52a
Abdelrahman Emara <abdelrahmanemara155@gmail.com>
2025-07-15 11:41:35 +0300

    Waiting some time before accepting the loan.

--- script.js
+++ script.js
@@ -263,7 +263,8 @@ btnLoan.addEventListener('click', function (e) {
 
   const amount = Math.floor(inputLoanAmount.value);
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
-    // Add movement
+    setTimeout(function () {
+      // Add movement
     currentAccount.movements.push(amount);
 
     // Add loan date
@@ -271,6 +272,7 @@ btnLoan.addEventListener('click', function (e) {
 
     // Update UI
     updateUI(currentAccount);
+    }, 2500);
   }
   inputLoanAmount.value = '';
 });
